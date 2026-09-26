1bea12928acf2c0a9e909d7ff010db929b381225
Devansh Srivastava <devanshsrivastava6306@gmail.com>
2026-09-26 18:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (93.57%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -6,7 +6,8 @@ class Solution {
             int mid = low+(high-low)/2;
             if (nums[mid] > nums[high]){
                 low = mid+1;
-            } else {
+            } 
+            else{
                 high = mid;
             }
         }
